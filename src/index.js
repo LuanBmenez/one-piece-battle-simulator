1048cb880f69b97548cc70f77f5810aaf8d73042
Luan Menezes <menezluan120@gmail.com>
2025-08-02 16:14:11 -0300

    Refatorei o playRaceFight para melhorar o registro e a estrutura
    Introduz um auxiliar LogRollResult para uma saída de rolagem de dados consistente e refatora playRaceFight para utilizá-lo. O código agora registra informações detalhadas de rolagem e atributos para cada rodada e tipo de luta, melhorando a clareza e a manutenibilidade.

--- src/index.js
+++ src/index.js
@@ -19,15 +19,15 @@ async function rollDice() {
 }
 
 async function getRandomFight() {
-  let random= Math.random()
-  let result 
+  let random = Math.random();
+  let result;
 
-  switch(true){
+  switch (true) {
     case random < 0.33:
-      result = "CORRIDA"
+      result = "CORRIDA";
       break;
     case random < 0.66:
-      result = "LUTA"
+      result = "LUTA";
       break;
     default:
       result = "AURA";
@@ -35,40 +35,96 @@ async function getRandomFight() {
   return result;
 }
 
-async function playRaceFight(character1, character2){
-for(let round = 1; round <=5; round++) {
-  console.log(`🥊rodada ${round}`);
-
-  // Style of fight
-  let fight = await getRandomFight();
-  console.log(` Luta sorteada: ${fight}`);
-
-  //roll dice
-  let diceResult1 = await rollDice();
-  let diceResult2 = await rollDice();
-
-  let totalTestSkill1 = 0;
-  let totalTestSkill2 = 0;
-if(fight === "CORRIDA"){
-  totalTestSkill1 = diceResult1 +character1.VELOCIDADE;
-  totalTestSkill2 = diceResult2 +character2.VELOCIDADE;
-
-  console.log(`${player1.NOME} rolou um dado de ${fight}: ${diceResult1}`)
-  console.log(`${player2.NOME} rolou um dado de ${fight}: ${diceResult2}`)
-}
-if(fight === "LUTA"){
-  totalTestSkill1 = diceResult1 +character1.FORÇA;
-  totalTestSkill2 = diceResult2 +character2.FORÇA;
-}
-if(fight === "AURA"){
-  totalTestSkill1 = diceResult1 +character1.HAKI;
-  totalTestSkill2 = diceResult2 +character2.HAKI;
-}
+async function LogRollResult(characterName, fight, diceResult, attribute) {
+  console.log(
+    `${characterName} 🎲 rolou um dado de ${fight}: ${diceResult} + ${attribute} = ${
+      diceResult + attribute
+    }`
+  );
 }
 
+async function playRaceFight(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🥊 rodada ${round}`);
+
+    // Style of fight
+    let fight = await getRandomFight();
+    console.log(` Estilo sorteada: ${fight}`);
+
+    //roll dice
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
+
+    let totalTestSkill1 = 0;
+    let totalTestSkill2 = 0;
+    if (fight === "CORRIDA") {
+      totalTestSkill1 = diceResult1 + character1.VELOCIDADE;
+      totalTestSkill2 = diceResult2 + character2.VELOCIDADE;
+
+      await LogRollResult(
+        character1.NOME,
+        "VELOCIDADE",
+        diceResult1,
+        character1.VELOCIDADE
+      );
+      await LogRollResult(
+        character2.NOME,
+        "VELOCIDADE",
+        diceResult2,
+        character2.VELOCIDADE
+      );
+    }
+    if (fight === "LUTA") {
+      totalTestSkill1 = diceResult1 + character1.FORÇA;
+      totalTestSkill2 = diceResult2 + character2.FORÇA;
+
+      await LogRollResult(
+        character1.NOME,
+        "FORÇA",
+        diceResult1,
+        character1.FORÇA
+      );
+
+      await LogRollResult(
+        character2.NOME,
+        "FORÇA",
+        diceResult2,
+        character2.FORÇA
+      );
+    }
+    if (fight === "AURA") {
+      totalTestSkill1 = diceResult1 + character1.HAKI;
+      totalTestSkill2 = diceResult2 + character2.HAKI;
+
+      console.log(`${character1.NOME} confrontou com ${character2.NOME}!🥊`)
+
+      await LogRollResult(
+        character1.NOME,
+        "HAKI",
+        diceResult1,
+        character1.HAKI
+      );
+      await LogRollResult(
+        character2.NOME,
+        "HAKI",
+        diceResult2,
+        character2.HAKI
+      );
+    }
+    if(totalTestSkill1> totalTestSkill2){
+      console.log(`${character1.NOME} marcou 1 ponto`)
+      character1.PONTOS++;
+    }else if(totalTestSkill2>totalTestSkill1 ){
+      console.log(`${character2.NOME} marcou 1 ponto`)
+      character2.PONTOS++;
+    }
+    console.log("_____________________________________")
+  }
 }
 
 (async function main() {
-  console.log(`🥊🚨Luta entre ${player1.NOME} e ${player2.NOME} começando ...\n`);
+  console.log(
+    `🥊🚨Luta entre ${player1.NOME} e ${player2.NOME} começando ...\n`
+  );
   await playRaceFight(player1, player2);
 })();
